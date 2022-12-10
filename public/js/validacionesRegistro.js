89a0b7319f998bcaaf5a10920a42829e6a889ba5
Nahuelminio <nahuelbenjamin04@gmail.com>
2022-12-10 02:22:54 -0300

    validaciones

--- public/js/validacionesRegistro.js
+++ public/js/validacionesRegistro.js
@@ -1,58 +1,14 @@
-window.addEventListener("load", function() {
-    let formulario = document.querySelector("#FormDicons")
-    console.log(formulario)
-    formulario.addEventListener("submit", function(e) {
-        let errores= [];
 
-        let campoNombre = document.querySelector("#Name");
+const nombre = document.getElementById ("Name")
+const apellido = document.getElementById ("apell")
+const mail = document.getElementById ("emailInput")
+const pass = document.getElementById ("passwordinput")
+const pais = document.getElementById ("Country")
+const error = document.getElementById("errores")
 
-        if (campoNombre.value == ""){
-            errores.push("El campo nombre tiene que estar completo");
-        } else if (campoNombre.value.length < 2){
-            errores.push ("El campo de nombre debe tener al menos 2 caracteres");
-        }
-
-        let campoApellido = document.querySelector("#apell");
-        
-        if (campoApellido.value == ""){
-            errores.push("El campo de apelido debe tener al menos 2 caracteres")
-        }
-
-        let campoNacimiento = document.querySelector("#Nacimiento")
-
-        if (campoNacimiento.value == "") {
-            errores.push("El campo de fecha tiene que estar completo");
-        }
-
-        let campoPais = document.querySelector("#Country")
-
-        if (campoPais.value == ""){
-            errores.push("El campo pais tiene que estar completo");
-        }
-
-        let campoEmail = document.querySelector("#emailInput")
-
-        if (campoEmail.value == ""){
-            errores.push("El campo email tiene que estar completo")
-        } else if (campoEmail.value.length < 2){
-            errores.push ("El campo de email debe tener al menos 2 caracteres");
-        }
-
-        let campoContraseña = document.querySelector("#passwordinput")
-
-        if (campoContraseña.value < 5){
-            errores.push ("El campo de contraseña debe tener al menos 5 caracteres");
-        }
-
-        if (errores.length > 0){
-            e.preventDefault()
-
-            let ulErrores = document.querySelector(".errores");
-            for (let i = 0; i < errores.length; i++) {
-
-                ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
-            }
-        }
-        
-    });
+form.addEventListener("submit", e=>{
+    e.preventDefauld()
+    if(nombre.value.length <3){
+        error += 'el nombre no es valido <br>'
+    }
 })
