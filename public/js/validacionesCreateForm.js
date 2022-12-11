f46f893e060163d9407ac4e7255b708dfe9e647a
Nahuelminio <nahuelbenjamin04@gmail.com>
2022-12-11 15:35:20 -0300

    subo validaciones de login, register, editFormy createForm

---
+++ public/js/validacionesCreateForm.js
@@ -0,0 +1,42 @@
+const nombre = document.querySelector ("#name")
+const precio = document.querySelector ("#price")
+const descripcion = document.querySelector ("#description")
+const img = document.getElementById("product-img")
+
+const validacionGeneral = (mensaje, e)=> {
+    const archivo = e.target;
+    const archivoValue = e.target.value;
+
+    if(archivoValue.trim().length == 0){
+        archivo.classList.add("campo-invalido")
+        archivo.nextElementSibling.classList.add("error")
+        archivo.nextElementSibling.innerText = mensaje
+
+    }else{
+        archivo.classList.remove("campo-invalido")
+        archivo.nextElementSibling.classList.remove("error")
+        archivo.nextElementSibling.innerText = ""   
+    }
+}
+
+img.addEventListener("change", (e) => {
+    const archivo= e.target;
+    const imgExt = e.target.files[0].name.split(".").pop().toLowerCase();
+    const extensiones = ["jpg", "jpeg", "png"]
+
+    if(!extensiones.includes(imgExt)) {
+        archivo.classList.add("campo-invalido")
+        archivo.nextElementSibling.classList.add("error")
+        archivo.nextElementSibling.innerText =  `Por favor ingresar una validacion valida: ${extensiones.join (", ")}`
+
+    }else{
+        archivo.classList.remove("campo-invalido")
+        archivo.nextElementSibling.classList.remove("error")
+        archivo.nextElementSibling.innerText = ""
+    }
+
+})
+
+nombre.addEventListener("blur", (e) => validacionGeneral("introduzca el nombre del producto" , e));
+precio.addEventListener("blur", (e) => validacionGeneral("introduzca el precio del producto" , e));
+descripcion.addEventListener("blur", (e) => validacionGeneral("introduzca la descripcion del producto" , e));
--- public/js/validacionesLogin.js
+++ public/js/validacionesLogin.js
@@ -1,25 +1,6 @@
-const nombre = document.querySelector ("#Name")
-const apellido = document.getElementById ("apell")
-const mail = document.getElementById ("emailInput")
-const pass = document.getElementById ("passwordinput")
-const pais = document.getElementById ("Country")
-const img = document.getElementById("avatar")
+const mail = document.getElementById ("email-login")
 
-const validacionGeneral = (mensaje, e)=> {
-    const archivo = e.target;
-    const archivoValue = e.target.value;        
-
-    if(archivoValue.trim().length == 0){
-        archivo.classList.add("campo-invalido")
-        archivo.nextElementSibling.classList.add("error")
-        archivo.nextElementSibling.innerText = mensaje
-
-    }else{
-        archivo.classList.remove("campo-invalido")
-        archivo.nextElementSibling.classList.remove("error")
-        archivo.nextElementSibling.innerText = ""
-    }
-}
+const pass = document.getElementById ("contraseña-login")
 
 const validacionContraseña = (mensaje, e)=> {
     const archivo = e.target;
@@ -37,6 +18,22 @@ const validacionContraseña = (mensaje, e)=> {
     }
 }
 
+
+const validacionGeneral = (mensaje, e)=> {
+    const archivo = e.target;
+    const archivoValue = e.target.value;
+
+    if(archivoValue.trim().length == 0){
+        archivo.classList.add("campo-invalido")
+        archivo.nextElementSibling.classList.add("error")
+        archivo.nextElementSibling.innerText = mensaje
+
+    }else{
+        archivo.classList.remove("campo-invalido")
+        archivo.nextElementSibling.classList.remove("error")
+        archivo.nextElementSibling.innerText = ""
+    }
+}
 
 
 const validacionEmail = e => {
@@ -58,36 +55,8 @@ const validacionEmail = e => {
 }
 
 
-
-img.addEventListener("change", (e) => {
-    const archivo= e.target;
-    const imgExt = e.target.files[0].name.split(".").pop().toLowerCase();
-    const extensiones = ["jpg", "jpeg", "png"]
-
-    if(!extensiones.includes(imgExt)) {
-        archivo.classList.add("campo-invalido")
-        archivo.nextElementSibling.classList.add("error")
-        archivo.nextElementSibling.innerText =  `Por favor ingresar una validacion valida: ${extensiones.join (", ")}`
-
-    }else{
-        archivo.classList.remove("campo-invalido")
-        archivo.nextElementSibling.classList.remove("error")
-        archivo.nextElementSibling.innerText = ""
-    }
-
-})
-
-
-nombre.addEventListener("blur", (e) => validacionGeneral("introduzca su nombre" , e));
-
-apellido.addEventListener("blur", (e) => validacionGeneral("introduzca su apellido" , e));
-
 mail.addEventListener("blur", (e) => validacionGeneral("introduzca su email" , e));
 
-
-
 pass.addEventListener("blur", (e) => validacionContraseña("introduzca una contraseña de 6 o mas caracteres" , e));
-
-pais.addEventListener("blur", (e) => validacionGeneral("introduzca su pais" , e));
 
 mail.addEventListener("input", validacionEmail);
