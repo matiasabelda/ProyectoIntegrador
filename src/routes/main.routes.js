9dcdc50a793984a1c328d495785b657de1312d89
Nahuelminio <nahuelbenjamin04@gmail.com>
2023-01-05 00:50:05 -0300

    subo documentacionApis.ejs

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', controladorMain.home);
-router.get('/search', controladorMain.search); 
+router.get('/search', controladorMain.search)
+router.get('/documentacionApis', controladorMain.documentacionApis); 
 
 module.exports = router;
